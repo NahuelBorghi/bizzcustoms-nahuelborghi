d8eafd2a7a842a555e17caebb8bb0cf95eee7500
Nahuel Borghi <69876615+NahuelBorghi@users.noreply.github.com>
2021-04-19 23:04:53 -0300

    creacion de archivos para tener los componentes separados en la carpeta components dentro de src

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import logo from './assets/img-pagina/logo.png';
-import carrito from './assets/images/carro.png';
 import './App.css';
-
+import React from "react"
+import Navbar from "./components/navbar"
+import Header from "./components/header"
 function App() {
   return (
-      <div id="body">
-        <div id="header">
-            <div><button id="tema" type="button">Tema Oscuro</button></div>
-            <div class="carrito"><a href="./views/carrito.html"><img src={carrito} alt="carrito"/></a></div>
+        <div id="body">
+        <Header/>
+        <Navbar/>
+        {bizzLogo()}
+        {footer()}
         </div>
-        <div class="logo"><a href="./"><img src={logo} alt="logo"/></a></div>
-        <div id="nav">
-            <ul>
-                <li><a href="./">Productos</a></li>
-                <li><a href="./">Compatibilidad</a></li>
-                <li><a href="./">Calcular Watts</a></li>
-                <li><a href="./">Contacto</a></li>
-            </ul>
-            <form id="search" action="" method="">
-                <input type="search" placeholder="Buscar..." name="buscar" id="buscar"/>
-            </form>
-        </div>
-        <div id="footer">
-            <div><a href="./views/sobre-nosotros.html">Sobre nosotros</a></div>
-            <div> Relleno </div>
-            <div><a href="./views/contacto.html">Contacto</a></div>
-        </div>
-    </div>
   );
+}
+function footer(){
+    return(
+        <div id="footer">
+            <div><a href="./">Sobre nosotros</a></div>
+            <div> Relleno </div>
+            <div><a href="./">Contacto</a></div>
+        </div>
+    );
+}
+function bizzLogo(){
+    return(
+        <div class="logo"><a href="./"><img src={logo} alt="logo"/></a></div>
+    );
 }
 
 export default App;
