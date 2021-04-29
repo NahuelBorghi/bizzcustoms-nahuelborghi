5338e607dfd2e7787b0d780cd74c0bb939a480db
Nahuel Borghi <69876615+NahuelBorghi@users.noreply.github.com>
2021-04-29 00:22:26 -0300

    Correccion ejercicio de CartItemCount. agregados los botones para sumar y restar del contador

--- src/components/Items/CartItemCount/CartItemCount.js
+++ src/components/Items/CartItemCount/CartItemCount.js
@@ -2,16 +2,18 @@ import { useState } from "react"
 import "./CartItemCount.scss"
 
 export const CartItemCount = () => {
-    const [cantidad,setCantidad] = useState()
+    const [cantidad,setCantidad] = useState(0)
     console.log(cantidad)
     const stock=10;
+    const suma = () => {if(cantidad<stock){setCantidad(cantidad+1)}}
+    const resta = () => {if(cantidad>1){setCantidad(cantidad-1)}}
     return(
         <div id="agregarCarrito">
             <p>carrito</p>
-            <form action="" name="cantidad" id="cantidad">
-                <input name="agregar" type="number" min="1" max={stock} onChange={(e) => setCantidad(e.target.value)}></input>
-                <input type="button" value="agregar al carrito"/>
-            </form>
+            <input name="agregar" type="number" value={cantidad} onChange={(e) => setCantidad(parseInt(e.target.value))}></input>
+            <button onClick={suma}>+</button>
+            <button onClick={resta}>-</button>
+            <input type="button" value="agregar al carrito"/>
         </div>
     )
 }
