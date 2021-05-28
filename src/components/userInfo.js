c5211d85c13416c8859ed51ce962ffff48ba3e87
Nahuel Borghi <69876615+NahuelBorghi@users.noreply.github.com>
2021-05-28 07:56:55 -0300

    listo creacion de ordenes y empezando a hacer el formulario de ingreso de datos de usuario para envio de la orden

---
+++ src/components/userInfo.js
@@ -0,0 +1,24 @@
+export const userInfo = () => {
+    const readInfo = () =>{
+        
+    }
+
+    return (
+        <>
+        <form>
+            <h3>direccion</h3>
+                <input></input>codigo postal
+                <input></input>provincia
+                <input></input>ciudad
+                <input></input>calle
+                <input></input>altura
+                <input></input>piso
+                <input></input>departamento
+            <input></input>mail
+            <input></input>nombre completo
+            <input></input>telefono
+            <button></button>guardar
+        </form>
+        </>
+    )
+}
